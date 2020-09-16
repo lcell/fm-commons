1229dbb4390df8d445fbe001f3dbad1f71bc61ef
saleson <qlichunyu@163.com>
2020-09-16 09:26:34 +0800

    .

--- fm-class-commons/src/main/java/com/github/saleson/fm/commons/ClassUtils.java
+++ fm-class-commons/src/main/java/com/github/saleson/fm/commons/ClassUtils.java
@@ -1,7 +1,32 @@
 package com.github.saleson.fm.commons;
 
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class ClassUtils {
 
+
+    public static final ParameterNameDiscoverer localVariableTableParameterNameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
+
+    public static String[] getParameterNames(Method method){
+        return getParameterNames(localVariableTableParameterNameDiscoverer, method);
+    }
+
+    public static String[] getParameterNames(Constructor<?> ctor){
+        return getParameterNames(localVariableTableParameterNameDiscoverer, ctor);
+    }
+
+
+    public static String[] getParameterNames(ParameterNameDiscoverer parameterNameDiscoverer, Method method){
+        return parameterNameDiscoverer.getParameterNames(method);
+    }
+
+    public static String[] getParameterNames(ParameterNameDiscoverer parameterNameDiscoverer, Constructor<?> ctor){
+        return parameterNameDiscoverer.getParameterNames(ctor);
+    }
 
 
 }
--- fm-class-commons/src/test/java/com/github/saleson/fm/mvc/scanner/EndPointScannerTest.java
+++ fm-class-commons/src/test/java/com/github/saleson/fm/mvc/scanner/EndPointScannerTest.java
@@ -3,9 +3,13 @@ package com.github.saleson.fm.mvc.scanner;
 import com.github.saleson.fm.enums.ClassType;
 import com.github.saleson.fm.scanner.EndPointScanner;
 import com.github.saleson.fm.scanner.SpringMVCEndPointScanner;
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Test;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Set;
 
 /**
@@ -19,5 +23,15 @@ public class EndPointScannerTest {
         EndPointScanner endPointScanner = new SpringMVCEndPointScanner(ClassType.ANNOTATION, RestController.class);
         Set<EndPointScanner.EndPoint> eps = endPointScanner.scan("com.github.saleson.fm.mvc.scanner");
         eps.forEach(System.out::println);
+    }
+
+    @Test
+    public void test2() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse("2019-05-23");
+        long day = 1000 * 60 * 60 * 24;
+        long mss = System.currentTimeMillis() - date.getTime();
+        System.out.println(mss/day);
+
     }
 }
